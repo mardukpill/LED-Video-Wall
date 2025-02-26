212b8eb1cfd01d4dfceb16e4d185fadfd7cb6014
Alex <asbohosi@buffalo.edu>
2025-02-26 17:48:07 -0500

    Inital code for tcp class.

--- server/tcp.cpp
+++ server/tcp.cpp
@@ -1,23 +1,29 @@
 #include "tcp.hpp"
+#include "client.hpp"
 #include <cstdint>
 #include <cstdio>
 #include <iostream>
 #include <iterator>
+#include <optional>
 #include <string.h>
 #include <netinet/in.h>
 #include <string>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <cstring>
+#include <map>
 
-int create_server_socket(uint32_t addr, uint16_t port) {
+const int MAX_WAITING_CLIENTS = 256;
+
+std::optional<LEDTCPServer> create_server(uint32_t addr, uint16_t port) {
     struct protoent* protocol_entry = getprotobyname("tcp");
     const int tcp_protocol_num = protocol_entry->p_proto;
     
     int server_socket = socket(AF_INET, SOCK_STREAM, tcp_protocol_num);
     if (server_socket == -1) {
         std::cerr << "Bad socket!\n";
-        return -1;
+        return std::nullopt;
     }
 
     struct sockaddr_in s_addr;
@@ -28,8 +34,41 @@ int create_server_socket(uint32_t addr, uint16_t port) {
     int res = bind(server_socket, (struct sockaddr *)&s_addr, sizeof(s_addr));
     if (res == -1) {
         std::cerr << "Failed bind: " << strerror(errno) << "\n";
-        return -1;
+        return std::nullopt;
     }
 
-    return server_socket;
+    return LEDTCPServer(addr, port, server_socket);
+}
+
+LEDTCPServer::LEDTCPServer(uint32_t addr, uint16_t port, int socket)
+    : addr(addr),
+      port(port),
+      socket(socket)
+{}
+
+void LEDTCPServer::wait_all_join(const std::vector<Client> clients) {
+    listen(this->socket, MAX_WAITING_CLIENTS);
+
+    std::map<uint64_t, Client> mac_to_client;
+    for (Client c : clients) {
+        mac_to_client[c.mac_addr] = c;
+    }
+
+    // keeps track of the clients that have the appropriate mac address
+    int accepted_clients = 0;
+    while (accepted_clients < clients.size()) {
+        int client_socket = accept(this->socket, NULL, NULL);
+        char check_in_buf[9];
+        recv(client_socket, check_in_buf, 12, 0);
+        uint16_t op_code;
+        std::memcpy(&op_code, &check_in_buf + 4, 2);
+        uint64_t mac_addr;
+        std::memcpy(&mac_addr, &check_in_buf + 6, 6);
+
+        auto c = mac_to_client.find(mac_addr); // c is an iterator
+        // if c is the value representing the end of the iterator, it is not present
+        if (!(mac_to_client.find(mac_addr) == mac_to_client.end())) {
+            // todo
+        }
+    }
 }
--- server/tcp.hpp
+++ server/tcp.hpp
@@ -11,7 +11,19 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h> // for close
+#include <vector>
+#include "client.hpp"
 
 int create_server_socket(uint32_t addr, uint16_t port);
+
+class LEDTCPServer {
+public:
+    uint32_t addr;
+    uint16_t port;
+    int socket;
+
+    LEDTCPServer(uint32_t addr, uint16_t port, int socket);
+    void wait_all_join(std::vector<Client> clients);
+};
 
 #endif
