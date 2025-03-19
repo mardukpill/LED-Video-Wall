2b92133496bea8918868bebf01b9ecbe812a7b4e
mike <mike@luxvera.com>
2025-03-19 18:12:54 -0400

    Add text type to element yaml parser

--- server/src/input-parser.cpp
+++ server/src/input-parser.cpp
@@ -1,9 +1,13 @@
+// clang-format off
 #include "canvas.h"
+#include "text-render.hpp"
 #include <iostream>
 #include <yaml-cpp/yaml.h>
 #include <string>
 #include <vector>
 
+// forward declarations
+cv::Scalar hexColorToScalar(const std::string &hexColor);
 
 std::vector<Element> parseInput(const std::string inputFile) {
     std::vector<Element> elementsVec;
@@ -24,6 +28,7 @@ std::vector<Element> parseInput(const std::string inputFile) {
             std::string type = value["type"].as<std::string>();
             int id = value["id"].as<int>();
 
+            // IMAGE TYPE
             if (type == "image") {
                 if (!value["filepath"] || !value["location"]) {
                     std::cerr << "Missing filepath or location for element: " << key << std::endl;
@@ -44,6 +49,34 @@ std::vector<Element> parseInput(const std::string inputFile) {
                 Element elem(filepath, id, loc);
                 elementsVec.push_back(elem);
             }
+            // TEXT TYPE
+            else if (type == "text") {
+                // check for necessary values
+                if (!value["content"] || !value["font_path"] || !value["location"] || !value["color"] || !value["size"]) 
+                {
+                    std::cerr << "Missing required values for element: " << key << std::endl;
+                    abort();
+                }
+                std::string filepath = value["font_path"].as<std::string>();
+                std::string content = value["content"].as<std::string>();
+                
+                int fontSize = value["size"].as<int>();
+                
+                // parse hex color to cv::Scalar
+                std::string hexColor = value["color"].as<std::string>();
+                cv::Scalar fontColor = hexColorToScalar(hexColor);
+                
+                // convert location from same format to cv::Point as used by renderTextToElement
+                std::vector<int> locVec = value["location"].as<std::vector<int>>();
+                if ((locVec.size() != 2) || (locVec.at(0) < 0) || (locVec.at(1) < 0)) {
+                    std::cerr << "Location for element " << key << " malformed." << std::endl;
+                    abort();
+                }
+                cv::Point posPoint(locVec.at(0), locVec.at(1));
+                
+                Element newElement = renderTextToElement(content, filepath, fontSize, fontColor, id, posPoint); 
+                elementsVec.push_back(newElement);
+            }
             else {
                 std::cerr << "Unsupported element type: " << type << std::endl;
             }
@@ -54,4 +87,17 @@ std::vector<Element> parseInput(const std::string inputFile) {
     }
 
     return elementsVec;
+}
+
+ 
+cv::Scalar hexColorToScalar(const std::string &hexColor) {
+  if (hexColor.length() != 7 || hexColor[0] != '#') {
+    // invalid, we'll just return black.
+    return cv::Scalar(0, 0, 0);
+  }
+
+  int r, g, b;
+  sscanf(hexColor.c_str(), "#%02x%02x%02x", &r, &g, &b);
+
+  return cv::Scalar(b, g, r);
 }
